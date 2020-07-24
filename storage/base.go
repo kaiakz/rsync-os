e414f0f670a3dd5cc082c6ac67b77bb3c936be18
kaiakz <kaiakx@yandex.com>
2020-07-24 19:10:17 +0800

    Updated interface: storage

--- storage/base.go
+++ storage/base.go
@@ -2,17 +2,18 @@ package storage
 
 import (
 	"io"
+	"os"
 	"rsync-os/rsync"
 )
 
 type FileMetadata struct {
 	Mtime int32
-	Mode  int32
+	Mode  os.FileMode
 }
 
 type IO interface {
-	Write(fileName string, reader io.Reader, fileSize int64, metadata FileMetadata) (n int64, err error)
+	Write(objectName string, reader io.Reader, objectSize int64, metadata FileMetadata) (n int64, err error)
 	//Read(fileName string, metadata FileMetadata)
-	Delete(fileName string) error
+	Delete(objectName string) error
 	List() rsync.FileList
 }
