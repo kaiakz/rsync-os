bf85a4b11565bcb2ea89f3d2217c00225ca0ee21
Kai <kaiakx@yandex.com>
2020-08-15 23:35:12 +0800

    Added comments

--- rsync/fs.go
+++ rsync/fs.go
@@ -10,6 +10,7 @@ type FileMetadata struct {
 	Mode  os.FileMode
 }
 
+// File System
 type FS interface {
 	Put(objectName string, reader io.Reader, objectSize int64, metadata FileMetadata) (n int64, err error)
 	//Get(fileName string, metadata FileMetadata)
